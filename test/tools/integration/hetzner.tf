caa9e558d044a8e474e00534cc9cfeeded4a9fcc
erdii <me@erdii.engineering>
2020-10-19 13:46:43 +0200

    fix hetzner terraform for tf 0.13

--- test/tools/integration/hetzner.tf
+++ test/tools/integration/hetzner.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.22.0"
+    }
+  }
+}
+
 provider "hcloud" {
   token = "${var.hcloud_token}"
 }
